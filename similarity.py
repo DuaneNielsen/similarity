c86a2c84d6f55831407c6e1d9d4189e6746d98eb
duanenielsen <duane.nielsen.rocks@gmail.com>
2020-01-27 23:06:51 -0800

    Implemented functions from https://papers.nips.cc/paper/8775-input-similarity-from-the-neural-network-perspective

--- similarity.py
+++ similarity.py
@@ -1,9 +1,80 @@
 import torch
 from datasets import package
 from torch.utils.data.dataloader import DataLoader
-from models import autoencoder, maker, similarity
+from models import autoencoder, maker
 from config import config
 from tqdm import tqdm
+
+eps = torch.finfo(torch.float).eps
+
+# pytorch can only compute the sum of gradients for each item in the minibatch
+# so vectorizing this code is not possible without implementing a custom backwards pass
+# see https://github.com/fKunstner/fast-individual-gradients-with-autodiff/tree/master/pytorch
+# for a few different methods to do this
+def compute_grads(f, x, return_output=False):
+    """
+
+    :param f: the NN to use for similarity computation
+    :param x: a batch N of inputs to compute grads for
+    :param return_output: return the outputs of the network
+    :return: (N, D, P) where
+    N is the number of inputs, D is the output dimension of the network, P is the total number of network parameters
+    """
+    y = f(x)
+    batch = []
+    for b in range(y.size(0)):
+        all_dim_grads = []
+        for i in range(y.size(1)):
+            f.zero_grad()
+            y[b, i].backward(retain_graph=True)
+            grads = torch.cat([t.grad.flatten(start_dim=0) for t in f.parameters()])
+            all_dim_grads.append(grads)
+
+        batch.append(torch.stack(all_dim_grads))
+    batch = torch.stack(batch)
+    if return_output:
+        return batch, y
+    else:
+        return batch
+
+
+def similarity_inner_product(f, x, y):
+    """
+    Computes the unnormalized distance between 2 batches of samples
+    :param f: the NN to use for similarity comparison
+    :param x: a batch of N samples
+    :param y: a batch of N samples to compare
+    :return: a matrix of D x D where D is the output dimension of the network
+    """
+    grad_x = compute_grads(f, x)
+    grad_y = compute_grads(f, y)
+    return torch.bmm(grad_x, grad_y.permute(0, 2, 1))
+
+
+def similarity_normed_inner_product(f, x, y):
+    """
+    Computes the unnormalized distance between 2 batches of samples
+    :param f: the NN to use for similarity comparison
+    :param x: a batch of N samples
+    :param y: a batch of N samples to compare
+    :return: N, D, D where D x D is a normalized correlation matrix, D = output dimension of the NN
+    """
+    grad_x = compute_grads(f, x)
+    grad_y = compute_grads(f, y)
+    normed_x = grad_x / (grad_x.norm(dim=2, keepdim=True) + eps)
+    normed_y = grad_y / (grad_y.norm(dim=2, keepdim=True) + eps)
+    return torch.bmm(normed_x, normed_y.permute(0, 2, 1))
+
+
+def similarity_trace_mean(f, x, y, return_diag=False):
+    kernel_matrix = similarity_normed_inner_product(f, x, y)
+    kernel_matrix_diag = kernel_matrix.diagonal(dim1=1, dim2=2)
+    trace_mean = torch.mean(kernel_matrix_diag, dim=1)
+    if return_diag:
+        return trace_mean, kernel_matrix_diag
+    else:
+        return trace_mean
+
 
 if __name__ == '__main__':
 
@@ -35,8 +106,10 @@ if __name__ == '__main__':
             length = min(len(batch[numeral_left]), len(batch[numeral_right]), 50)
             left = torch.stack(batch[numeral_left][0:length]).to(args.device)
             right = torch.stack(batch[numeral_right][0:length]).to(args.device)
-            sim_stats[numeral_left, numeral_right] = similarity.similarity_trace_mean(auto_encoder.encoder,
-                                                                                      left.flatten(start_dim=1),
-                                                                                      right.flatten(start_dim=1)).mean()
+            sim_stats[numeral_left, numeral_right] = similarity_trace_mean(auto_encoder.encoder,
+                                                                           left.flatten(start_dim=1),
+                                                                           right.flatten(start_dim=1)).mean()
     torch.set_printoptions(sci_mode=False)
     print(sim_stats)
+
+
--- tests/test_similiarty.py
+++ tests/test_similiarty.py
@@ -1,16 +1,17 @@
 import torch
 import torch.nn as nn
 import models.similarity as m
+import similarity
 
 x = torch.rand(4, 2)
 f = nn.Sequential(nn.Linear(2, 2), nn.ReLU(), nn.Linear(2, 2))
-grads = m.compute_grads(f, x)
+grads = similarity.compute_grads(f, x)
 
 
 def test_comp_grads():
     x = torch.rand(4, 2)
     f = nn.Sequential(nn.Linear(2, 2), nn.ReLU(), nn.Linear(2, 2))
-    grads = m.compute_grads(f, x)
+    grads = similarity.compute_grads(f, x)
     assert grads.size(0) == 4
     assert grads.size(1) == 2
     assert grads.size(2) == 2
@@ -19,14 +20,14 @@ def test_comp_grads():
 def test_inner_prod():
     x = torch.rand(4, 2)
     f = nn.Sequential(nn.Linear(2, 2, bias=False), nn.ReLU(), nn.Linear(2, 1, bias=False))
-    sip = m.similarity_inner_product(f, x, x)
+    sip = similarity.similarity_inner_product(f, x, x)
     print(sip)
 
 
 def test_normed_inner_prod():
     x = torch.randn(40, 16)
     f = nn.Sequential(nn.Linear(16, 8), nn.Sigmoid(), nn.Linear(8, 4))
-    snip = m.similarity_normed_inner_product(f, x, x)
+    snip = similarity.similarity_normed_inner_product(f, x, x)
     snip = snip.diagonal(dim1=1, dim2=2)
     assert torch.allclose(torch.ones_like(snip), snip)
 
@@ -35,12 +36,12 @@ def test_normed_inner_prod_dist():
     x = torch.randn(40, 16)
     y = torch.randn(40, 16)
     f = nn.Sequential(nn.Linear(16, 8), nn.Sigmoid(), nn.Linear(8, 4))
-    snip = m.similarity_normed_inner_product(f, x, y)
+    snip = similarity.similarity_normed_inner_product(f, x, y)
     print(snip)
 
 
 def test_trace_mean():
     x = torch.randn(40, 16)
     f = nn.Sequential(nn.Linear(16, 8), nn.Sigmoid(), nn.Linear(8, 4))
-    dist = m.similarity_trace_mean(f, x, x)
+    dist = similarity.similarity_trace_mean(f, x, x)
     print(dist)
